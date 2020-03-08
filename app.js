a4140c776d6f199963f40e302934bfa588d25a49
unknown <hamza.emad.jaradat.4@gmail.com>
2020-03-08 10:57:42 +0200

    Port

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ io.on('connection', function(socket){
 
 
 });
-
-http.listen(process.env.PORT||3000, function(){
+let port  = process.env.PORT||3000
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
 
